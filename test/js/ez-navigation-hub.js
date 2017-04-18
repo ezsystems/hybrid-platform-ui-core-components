78b57ea017a66289528c928fe12b7bf12ef343f5
Damien Pobel <dp@ez.no>
2017-04-18 17:17:43 +0200

    Configured eslint to check JS files

--- test/js/ez-navigation-hub.js
+++ test/js/ez-navigation-hub.js
@@ -1,7 +1,7 @@
 /* global describe, it, assert, fixture */
 describe('ez-navigation-hub', function() {
     it('should be defined', function () {
-        var element = fixture('BasicTestFixture');
+        const element = fixture('BasicTestFixture');
 
         assert.equal(
             window.customElements.get('ez-navigation-hub'),
--- test/js/ez-platform-ui-app.js
+++ test/js/ez-platform-ui-app.js
@@ -1,7 +1,7 @@
 /* global describe, it, assert, fixture */
 describe('ez-platform-ui-app', function() {
     it('should be defined', function () {
-        var element = fixture('BasicTestFixture');
+        const element = fixture('BasicTestFixture');
 
         assert.equal(
             window.customElements.get('ez-platform-ui-app'),
