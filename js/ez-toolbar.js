4bf0971b288b5b4368cca954deb146fc8956521b
stephaneDiot <stephane.diot@ez.no>
2017-04-27 17:57:23 +0200

    EZP-27269: Implement toolbar component

--- js/ez-toolbar.js
+++ js/ez-toolbar.js
@@ -1,8 +1,27 @@
 (function () {
-    /** @polymerElement */
+    /**
+    * <ez-toolbar> represents the toolbar of the application.
+    * It can be visible or not.
+    *
+    * @polymerElement
+    * @demo demo/ez-toolbar.html
+    */
     class EzToolbar extends Polymer.Element {
         static get is() {
             return 'ez-toolbar';
+        }
+
+        static get properties() {
+            return {
+                /**
+                 * Indicates whether the toolbar is visible or not.
+                 */
+                visible: {
+                    type: Boolean,
+                    reflectToAttribute: true,
+                    value: false,
+                },
+            };
         }
     }
 
