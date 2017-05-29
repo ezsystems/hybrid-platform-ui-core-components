36d5b536f05d1fe223f86d2f4449f90bdd8a1d97
stephaneDiot <stephane.diot@ez.no>
2017-05-29 15:51:17 +0200

    EZP-27268: Implement navigation hub

--- js/ez-navigation-hub.js
+++ js/ez-navigation-hub.js
@@ -1,10 +1,161 @@
 (function () {
-    /** @polymerElement */
-    class EzNavigationHub extends Polymer.Element {
+    /**
+    * <ez-navigation-hub> represents the navigation hub of the application.
+    * Zones can be selected to display the links refering to the different parts of the application.
+    *
+    * Zones in the navigation hub are identified by a data-zone-identifier containing the zone identifier.
+    * Example :
+    *
+    *    <ul>
+    *      <li data-zone-identifier="1">
+    *        <a href="#">zone1</a>
+    *      </li>
+    *    </ul>
+    *
+    * To appear in a zone, a link should have a data-zone attribute containing the corresponding zone identifier.
+    * In addition, such links should have the class ez-zone-link.
+    * Example :
+    *
+    *   <ul class="ez-navigation">
+    *     <li data-zone="1" class="ez-zone-link">
+    *       <a href="url">link</a>
+    *     </li>
+    *   </ul>
+    *
+    * This component relies on`Element.matches` that is not available in Edge 14.
+    * So for this component to work in this browser, the page should include a polyfill for this standard API.
+    * @polymerElement
+    * @demo demo/ez-navigation-hub.html
+    */
+    class NavigationHub extends Polymer.Element {
         static get is() {
             return 'ez-navigation-hub';
         }
-    }
+        static get properties() {
+            return {
+                'activeZoneClass': {
+                    type: String,
+                    value: 'ez-active-zone',
+                },
+                'matchedLinkClass': {
+                    type: String,
+                    value: 'ez-matched-link',
+                },
+                'activeZone': {
+                    type: String,
+                    observer: '_updateActiveZone',
+                },
+                'matchedLinkUrl': {
+                    type: String,
+                    observer: '_highlightLink',
+                },
+            };
+        }
 
-    window.customElements.define(EzNavigationHub.is, EzNavigationHub);
+        connectedCallback() {
+            super.connectedCallback();
+            this.addEventListener('click', (e) => {
+                if ( e.target.matches('[data-zone-identifier] a') ) {
+                    e.preventDefault();
+                    this.activeZone = e.target.parentNode.getAttribute('data-zone-identifier');
+                }
+            });
+        }
+
+        /**
+         * Highlights the active zone and updates navigation by showing the related links.
+         * It's an observer of the 'activeZone' property.
+         *
+         * @param {String} newValue
+         * @param {String} oldValue
+         */
+        _updateActiveZone(newValue, oldValue) {
+            this._highlightZone(oldValue, newValue);
+            this._updateNavigation(oldValue, newValue);
+        }
+
+        /**
+         * Returns the zone corresponding to the given identifier.
+         *
+         * @param {String} identifier
+         * @return {HTMLElement}
+         */
+        _getZone(identifier) {
+            return this.querySelector('[data-zone-identifier="' + identifier + '"]');
+        }
+
+        /**
+         * Highlights the current active zone and unhighlight the previous one.
+         *
+         * @param {String} oldZoneIdentifier
+         * @param {String} newZoneIdentifier
+         */
+        _highlightZone(oldZoneIdentifier, newZoneIdentifier) {
+            if ( oldZoneIdentifier ) {
+                this._getZone(oldZoneIdentifier).classList.remove(this.activeZoneClass);
+            }
+            if ( newZoneIdentifier ) {
+                this._getZone(newZoneIdentifier).classList.add(this.activeZoneClass);
+            }
+        }
+
+        /**
+         * Returns every items corresponding to the given zone identifier.
+         *
+         * @param {String} identifier
+         * @return {NodeList}
+         */
+        _getZoneItems(identifier) {
+            return this.querySelectorAll('[data-zone="' + identifier + '"]');
+        }
+
+        /**
+         * Updates navigation by showing the items of the current active zone
+         * and removing the ones of the previous zone.
+         *
+         * @param {String} oldZoneIdentifier
+         * @param {String} newZoneIdentifier
+         */
+        _updateNavigation(oldZoneIdentifier, newZoneIdentifier) {
+            const forEach = Array.prototype.forEach;
+            const isInActiveZoneClass = 'is-in-active-zone';
+
+            if ( oldZoneIdentifier ) {
+                forEach.call(this._getZoneItems(oldZoneIdentifier), function (link) {
+                    link.classList.remove(isInActiveZoneClass);
+                });
+            }
+            if ( newZoneIdentifier ) {
+                forEach.call(this._getZoneItems(newZoneIdentifier), function (link) {
+                    link.classList.add(isInActiveZoneClass);
+                });
+            }
+        }
+
+        /**
+         * Returns the node by the given url
+         *
+         * @param {String} url
+         * @return {HTMLElement}
+         */
+        _getItemByUrl(url) {
+            return this.querySelector('[href="' + url + '"]').parentNode;
+        }
+
+        /**
+         * Highlights the link corresponding to the given url and unhighlight the previous one.
+         *
+         * @param {String} newUrl
+         * @param {String} oldUrl
+         */
+        _highlightLink(newUrl, oldUrl) {
+            if ( oldUrl ) {
+                this._getItemByUrl(oldUrl).classList.remove(this.matchedLinkClass);
+            }
+            if ( newUrl ) {
+                this._getItemByUrl(newUrl).classList.add(this.matchedLinkClass);
+            }
+        }
+    }
+    window.customElements.define(NavigationHub.is, NavigationHub);
 })();
--- test/js/ez-navigation-hub.js
+++ test/js/ez-navigation-hub.js
@@ -1,4 +1,10 @@
 describe('ez-navigation-hub', function() {
+    let element;
+
+    beforeEach(function () {
+        element = fixture('BasicTestFixture');
+    });
+
     it('should be defined', function () {
         const element = fixture('BasicTestFixture');
 
@@ -6,5 +12,136 @@ describe('ez-navigation-hub', function() {
             window.customElements.get('ez-navigation-hub'),
             element.constructor
         );
+    });
+
+    describe('properties', function () {
+        describe('`activeZoneClass`', function () {
+            it('should have a default value', function () {
+                assert.equal(element.activeZoneClass, 'ez-active-zone');
+            });
+
+            it('should be set by the attribute', function () {
+                const value = 'Something';
+
+                element.setAttribute('active-zone-class', value);
+                assert.equal(element.activeZoneClass, value);
+            });
+        });
+
+        describe('`matchedLinkClass`', function () {
+            it('should have a default value', function () {
+                assert.equal(element.matchedLinkClass, 'ez-matched-link');
+            });
+
+            it('should be set by the attribute', function () {
+                const value = 'Something';
+
+                element.setAttribute('matched-link-class', value);
+                assert.equal(element.matchedLinkClass, value);
+            });
+        });
+
+        describe('`activeZone`', function () {
+            it('should default to undefined', function () {
+                assert.isUndefined(element.activeZone);
+            });
+
+            it('should be set by the attribute', function () {
+                const value = 'contents';
+
+                element.setAttribute('active-zone', value);
+                assert.equal(element.activeZone, value);
+            });
+        });
+
+        describe('`matchedLinkUrl`', function () {
+            it('should default to undefined', function () {
+                assert.isUndefined(element.matchedLinkUrl);
+            });
+
+            it('should be set by the attribute', function () {
+                const value = 'url1';
+
+                element.setAttribute('matched-link-url', value);
+                assert.equal(element.matchedLinkUrl, value);
+            });
+        });
+    });
+
+    describe('active zone update', function () {
+        function simulateClick(element) {
+            const click = new CustomEvent('click', {
+                bubbles: true,
+                cancelable: true,
+            });
+
+            element.dispatchEvent(click);
+            return click;
+        }
+        function assertEventHandled(event) {
+            assert.ok(
+                event.defaultPrevented,
+                'The event should have been prevented'
+            );
+        }
+
+        describe('highlight zone', function () {
+            it('should highlight the selected zone', function () {
+                const zone = element.querySelector('[data-zone-identifier="contents"]');
+                const link = zone.querySelector('a');
+                const event = simulateClick(link);
+
+                assertEventHandled(event);
+                assert.isOk(zone.classList.contains(element.activeZoneClass));
+            });
+
+            it('should switch zone highlight', function () {
+                const contentsZone = element.querySelector('[data-zone-identifier="contents"]');
+                const otherZone = element.querySelector('[data-zone-identifier="other"]');
+                const link = otherZone.querySelector('a');
+
+                element.activeZone = 'contents';
+                const event = simulateClick(link);
+
+                assertEventHandled(event);
+
+                assert.isNotOk(contentsZone.classList.contains(element.activeZoneClass), 'zone `contents` should not be highlighted');
+                assert.isOk(otherZone.classList.contains(element.activeZoneClass), 'zone `other` should be highlighted');
+            });
+        });
+
+        describe('update navigation', function () {
+            it('should display links belonging to the active zone', function () {
+                const link = element.querySelector('[data-zone-identifier="contents"] a');
+                const event = simulateClick(link);
+
+                assertEventHandled(event);
+                assert.isNotOk(element.querySelector('[data-zone="other"]').classList.contains('is-in-active-zone'), 'zone `other` links should not be displayed');
+                assert.isOk(element.querySelector('[data-zone="contents"]').classList.contains('is-in-active-zone'), 'zone `contents` links should be displayed');
+            });
+
+            it('should switch links displayed', function () {
+                const link = element.querySelector('[data-zone-identifier="other"] a');
+                const event = simulateClick(link);
+
+                assertEventHandled(event);
+                assert.isNotOk(element.querySelector('[data-zone="contents"]').classList.contains('is-in-active-zone'), 'zone `content` links should not be displayed');
+                assert.isOk(element.querySelector('[data-zone="other"]').classList.contains('is-in-active-zone'), 'zone `other` links should be displayed');
+            });
+        });
+    });
+
+    describe('match link url', function () {
+        it('should highlight the selected link', function () {
+            element.matchedLinkUrl = 'url1';
+            assert.isOk(element.querySelector('[href="url1"]').parentNode.classList.contains(element.matchedLinkClass), 'link1 should be highlighted');
+        });
+
+        it('should switch the highlighted link', function () {
+            element.matchedLinkUrl = 'url1';
+            element.matchedLinkUrl = 'url2';
+            assert.isNotOk(element.querySelector('[href="url1"]').parentNode.classList.contains(element.matchedLinkClass), 'link1 should not be highlighted anymore');
+            assert.isOk(element.querySelector('[href="url2"]').parentNode.classList.contains(element.matchedLinkClass), 'link2 should be highlighted');
+        });
     });
 });
