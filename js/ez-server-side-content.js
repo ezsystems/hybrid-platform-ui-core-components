6c334bbe1aef5ae2ca7f48dc9deada392b58285a
Damien Pobel <dp@ez.no>
2017-06-22 15:31:48 +0200

    EZP-27482: Added closest polyfill to ez-server-side-content tests

--- js/ez-server-side-content.js
+++ js/ez-server-side-content.js
@@ -5,6 +5,11 @@
      * generated content. For now, this class is able to handle the tabs markup
      * thanks to the eZ.TabsMixin.
      *
+     * Among others standard APIs, this component relies on `Element.closest`.
+     * `Element.closest` is not available in Edge 14. So for this component to
+     * work in this browser, the page should include a polyfill for this
+     * standard API.
+     *
      * @polymerElement
      * @demo demo/ez-server-side-content.html
      */
