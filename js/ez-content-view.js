a34f8f25f0c4506709153101f7cc1f860c8d0965
Damien Pobel <dp@ez.no>
2017-08-29 12:56:43 +0200

    EZP-27586: Refreshes subitems if default ordering was updated

--- js/ez-content-view.js
+++ js/ez-content-view.js
@@ -10,6 +10,36 @@
             return 'ez-content-view';
         }
 
+        connectedCallback() {
+            super.connectedCallback();
+            this._setupSubitemRefresh();
+        }
+
+        /**
+         * Sets up a listener on `ez:asynchronousBlock:updated` to check whether
+         * the default subitem ordering was changed. If it was, the subitems are
+         * refreshed.
+         */
+        _setupSubitemRefresh() {
+            this.addEventListener('ez:asynchronousBlock:updated', (e) => {
+                const source = e.detail.source;
+
+                if ( source instanceof HTMLFormElement && source.name === 'ordering' ) {
+                    this._refreshSubitem();
+                }
+            });
+        }
+
+        /**
+         * Refreshes the subitem list (if any)
+         */
+        _refreshSubitem() {
+            const subitem = this.querySelector('ez-subitem');
+
+            if ( subitem ) {
+                subitem.refresh();
+            }
+        }
     }
 
     customElements.define(ContentView.is, ContentView);
